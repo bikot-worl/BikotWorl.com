5027b4b4970449503317dd05d1d1b66558bc45d2
Bikot <bikot.worl@gmail.com>
2026-07-20 06:20:45 -0600

    Add files via upload

--- Web/script.js
+++ Web/script.js
@@ -6,14 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then((registration) => {
-                console.log('Service Worker registrado con éxito:', registration.scope);
-            })
-            .catch((error) => {
-                console.error('Error al registrar el Service Worker:', error);
-            });
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+            registration.unregister();
+        }
     });
 }
 
